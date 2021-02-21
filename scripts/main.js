af178bbc5470e6127bff39ae5219ea962eeb37c3
Danny JeanLouis <dlouis3386@gmail.com>
2021-02-21 04:02:44 -0500

    added navigation: mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-console.log("prayer")
+const menuOpen = document.querySelector(".navbar__menu-open");
+const menuClose = document.querySelector(".navbar__menu-close");
+// const menuIcons = document.querySelectorAll(".menu-icon");
+
+menuOpen.addEventListener("click", (evt) => {
+  menuOpen.style.display = "none";
+  menuClose.style.display = "block";
+});
+
+menuClose.addEventListener("click", (evt) => {
+  menuClose.style.display = "none";
+  menuOpen.style.display = "block";
+});
